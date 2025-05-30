cf1ecfed7d58390b6bcb4a24510138dc19e58ee7
Harold Areiza <haroldareiza89@gmail.com>
2025-05-29 19:45:06 -0500

    diseño inicial

--- BillCalculator/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BillCalculator.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
